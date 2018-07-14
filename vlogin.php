5875aa24dc2819d6fa6df2d24128694bc43ce160
mounika <mounika.reddy0611@gmail.com>
2018-07-15 03:54:19 +0530

    vlogin mounika

--- vlogin.php
+++ vlogin.php
@@ -1,32 +1,24 @@
-<?
-session_start();
+<?php
+
 require_once("DBConn.php");
-$email = $_POST[''];
-$pwd = $_POST[''];
-$sql = mysqli_query($conn,"select vid,name,email,pwd,phoneno from volunteer where email='".$email."' and pwd='".$pwd."'") or die(mysql_error());
-$res=mysqli_num_rows($sql);
-$message="Wrong Email or Password";
-if($res>0)
-	{
-        $row=mysqli_fetch_assoc($sql);
-		$_SESSION['email']=$email;
-		$_SESSION['vid']=$row['cid'];
-        $_SESSION['name']=$row['name'];
-		echo $_SESSION['email']."<br>";
-		echo $_SESSION['vid']."<br>";
-		echo $_SESSION['name']."<br>";
-        //echo $_SESSION['phno'];
-		//require_once("../os.php");
-		if(isset($_SESSION['REQUEST']) && $_SESSION['REQUEST']===TRUE)
-		{
-			echo "HI";
-			header("location:volunteerhome.php");
-		}
-		else{
-			header("location:index.php");
-		}
-	}
-	else{
-		echo "<script type='text/javascript'>alert('$message');window.location='index.php'</script>";
-	}
+session_start();
+$email = $_POST['email'];
+$pwd = $_POST['password'];
+// echo $pwd;
+// echo $email;
+
+$sql=mysqli_query($conn,"select vid,email,pwd,name from volunteer where email='".$email."' and pwd='".$pwd."'") or die(mysql_error());
+$count = mysqli_num_rows($sql);
+if($count>0){
+	$row=mysqli_fetch_assoc($sql);
+	$_SESSION['vid']=$row['vid'];
+	$_SESSION['name']=$row['name'];
+	header("location:volunteerhome.php");
+	// echo $_SESSION['name'];
+	// echo "success";
+}
+else{
+	
+	echo "<script type='text/javascript'>alert('Invalid');window.location='index.php'</script>";
+}
 ?>
