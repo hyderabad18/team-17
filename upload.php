204133783f327cd9eb97d6bc0b849fb2769554a8
nikhil jella <nikhilpateljella@gmail.com>
2018-07-14 20:54:56 +0530

    created sql query to send data to

--- upload.php
+++ upload.php
@@ -1,4 +1,10 @@
 <?php
+//updated
+$conn = new mysqli('localhost','root','','sharenotes');
+//upto here
+
+
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -16,8 +22,19 @@ if($_FILES["fileToUpload"]["size"] > 50000000 and $uploadOk ==1 ){
 if($uploadOk == 1){
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    //updated   
+    $sql_upload = "INSERT INTO table_name VALUES ('','$target_file','$imageFileType')";
+    $conn->query($sql_upload);    
+    //upto here
+
 } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
+
+
+
+
+
+
 ?>
