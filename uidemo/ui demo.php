91ed7031869a4af93be44171edf8567ff755bb71
Lahari <laharichowdary49@gmail.com>
2018-07-15 07:45:55 +0530

    commit

---
+++ uidemo/ui demo.php
@@ -0,0 +1,234 @@
+﻿<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	
+
+
+		
+		<title>Education is basic right for Everyone</title>
+		<body background ="book.jpeg" height="200" width="400" >
+		
+
+
+
+
+
+		<style type="text/css">
+		
+		
+		
+		
+
+		h1 {
+    text-shadow: 2px 2px #e056fd;
+		}
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+         
+		
+		body {
+			background-color: #ffcccc;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+			height: 1000px; 
+			max-height: 1500px; 
+			font-family:Sans-serif;
+			line-height: 1.5em;
+			
+		}
+		.topnav {
+  overflow: hidden;
+  background-color: #6F1E51;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+		
+		
+		#header {
+			vertical-align:middle;
+	        
+            top:200px;			
+			
+			overflow: hidden; /* Disables scrollbars on the header frame. To enable scrollbars, change "hidden" to "scroll" */
+			
+			
+		}
+		
+		
+		#nav {
+			position: absolute; 
+			top: 100px; 
+			left: 0; 
+			bottom: 0;
+			width: 230px;
+			overflow: auto; /* Scrollbars will appear on this frame only when there's enough content to require scrolling. To disable scrollbars, change to "hidden", or use "scroll" to enable permanent scrollbars */
+			background: #dff9fb; 		
+		}
+		
+		#logo {
+			
+		}
+		
+		main {
+			position: fixed;
+			top: 200px; /* Set this to the height of the header */
+			left: 230px; 
+			right: 0;
+			bottom:0;
+			overflow: auto; 
+			background:#F8EFBA ;
+		}
+		#footer{
+background-color:#000;
+position:relative;
+height:50px;
+
+
+
+clear:both;
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+
+
+		
+		.innertube {
+			margin: 15px;	/* Provides padding for the content */
+		}
+		
+		p {
+			color:#e84118 ;
+			background-color:#2f3640;
+		}
+
+		nav ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+		}
+		
+		nav ul a {
+			color: #dff9fb;
+			text-decoration: none;
+		}
+				
+		/*IE6 fix*/
+		* html body{
+			padding: 200px 0 0 230px; /* Set the first value to the height of the header and last value to the width of the nav */
+		}
+		
+		* html main{ 
+			height: 2054px; 
+			width: 100%; 
+		}
+		.head{
+			vertical-align:middle;
+		}
+		
+		</style>
+		
+		
+	
+	</head>
+	
+	<body>		
+	
+	
+
+		<header id="header">
+			<div id="logo">
+			<img src="logo.jpg" height="50" width="60" style="float:left">
+			
+			<!DOCTYPE html>
+			
+			<div class="topnav">
+                 <a class="active" href="#home">Home</a>
+                 <a href="#news">News</a>
+                  <a href="#contact">Contact</a>
+                  <a href="#about">About</a>
+                </div>
+
+			
+			
+
+
+
+			<h1 align="center" style="font-size:50px;font-weight: bold;color:#130f40 " class="head">I AND EYE</h1>
+			
+               
+</div>
+<nav id="nav">
+			<div class="innertube">
+				<h1>Explore</h1>
+				<ul>
+					<li><a href="register.php">REGISTER</a></li>
+					<li><a href="indexx.php">LOGIN</a></li>
+					<li><a href="home.php">HOME<a></li>
+					<li><a href="credit.php">Payment<a></li>
+				</ul>
+			
+			
+</div>
+
+
+		</nav>	
+		</header>
+		
+				
+		<main>
+		
+		
+	
+		
+		
+			<div class="innertube">
+			<h1 style="font-size:30px">ABOUT US</h1>
+				<img src="blind.jpeg" height="200" width="300" style="float:right">
+				<p>The UNESCO stated “education for sustainable development is a broad task that calls for the full involvement of multiple educational organizations and groups in bureaucracies and civil societies. These include Non-Governmental Organizations or NGOs[1].
+
+The language of education used by nation-states as well as international, intergovernmental organizations, non-governmental organizations, also known as NGOs, (both transnational and national), and agents of civil society (many of which belong to the aforementioned categories) contributes heavily to the self-identification of individuals. NGOs, can be defined as "formal organizations, and as such, they emerge when a group of people organize themselves into a social unit “that was established with the explicit objective of achieving certain ends and formulating rules to govern the relations among the members of the organization and the duties of each member” (Blau and Scott, 1970)".[2]
+
+</p>
+				
+			</div>
+			<h1>WHAT WE DO</h1>
+			
+			
+			
+			<div id="container">
+<div id="main">
+
+</div>
+</div>
+<footer id="footer">
+</footer>
+			
+
+
+			
+		</main>
+
+	</body>
+</html>
